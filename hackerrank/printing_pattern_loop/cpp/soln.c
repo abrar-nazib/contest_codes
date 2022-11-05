1bdfdfbcf020b45c5a88a512f89e9127da9c5b4c
abrar-nazib <abrarnazib@gmail.com>
2022-11-05 14:40:23 +0600

    Starting prep for codespark

--- hackerrank/printing_pattern_loop/cpp/soln.c
+++ hackerrank/printing_pattern_loop/cpp/soln.c
@@ -1,24 +1,25 @@
-// #include <bits/stdc++.h>
-// using namespace std;
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-
-#define _DEBUG 1
 
 int main()
 {
-#ifdef _DEBUG
+
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-#endif
 
-    int n;
+    int n, h_min, v_min, min;
     scanf("%d", &n);
-    int range = (2 * n) - 1;
-    for (int i = 0; i < range; i++)
+    int len = (n * 2) - 1;
+
+    for (int i = 1; i <= len; i++)
     {
-        return;
+        for (int j = 1; j <= len; j++)
+        {
+            v_min = i <= len - i ? i - 1 : len - i;
+            h_min = j <= len - j ? j - 1 : len - j;
+            min = v_min <= h_min ? v_min : h_min;
+            printf("%d ", n - min);
+        }
+        printf("\n");
     }
     return 0;
 }
