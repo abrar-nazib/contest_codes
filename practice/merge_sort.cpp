a1dc08d6e86289fcdf142bb3ddeacf458abd2ab9
abrar-nazib <abrarnazib@gmail.com>
2023-01-12 19:23:17 +0600

    sort and merge sort done

--- practice/merge_sort.cpp
+++ practice/merge_sort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 const int N = 1e5 + 10;
 int arr[N];
 
-int merge(int l, int r, int mid)
+void merge(int l, int r, int mid)
 {
     int l_size = mid - l + 1; // left size will include mid
     int l_arr[l_size + 1];    // +1 because last element will store int_max as extra.
@@ -29,7 +29,27 @@ int merge(int l, int r, int mid)
     int r_i = 0;
     for (int i = l; i <= r; i++)
     {
+        if (l_arr[l_i] <= r_arr[r_i])
+        {
+            arr[i] = l_arr[l_i];
+            l_i++;
+        }
+        else
+        {
+            arr[i] = r_arr[r_i];
+            r_i++;
+        }
     }
+}
+
+void merge_sort(int l, int r)
+{
+    if (l == r) // if the array size is 1 -> array with single element
+        return;
+    int mid = (l + r) / 2;
+    merge_sort(l, mid);
+    merge_sort(mid + 1, r);
+    merge(l, r, mid);
 }
 
 int main()
@@ -38,6 +58,27 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    int n, x;
+    cin >> n;
+    int l = 0;
+    int r = n - 1;
+    while (n--)
+    {
+        cin >> x;
+        arr[n] = x;
+    }
+    for (int i = 0; i <= r; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    merge_sort(l, r);
+    for (int i = 0; i <= r; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
