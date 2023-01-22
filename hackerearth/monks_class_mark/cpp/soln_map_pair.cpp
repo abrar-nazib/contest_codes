552724fa1fcd8b1a54ae39b608625c43ce1d1e4c
abrar-nazib <abrarnazib@gmail.com>
2023-01-22 19:02:38 +0600

    kisui korte parinai ajke lol

---
+++ hackerearth/monks_class_mark/cpp/soln_map_pair.cpp
@@ -0,0 +1,37 @@
+
+// Problem: The Monk and Class Marks
+// Link: https://www.hackerearth.com/problem/algorithm/the-monk-and-class-marks
+// Date: 22-01-2023
+// Author: Nazib Abrar
+
+#define _LOCAL 1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifdef _LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    unsigned long long test_cases, number;
+    string name;
+    multiset<pair<int, string>> result;
+    cin >> test_cases;
+    while (test_cases--)
+    {
+        cin >> name >> number;
+        result.insert(make_pair(-1 * number, name));
+    }
+    for (auto elem : result)
+    {
+        cout << elem.second << " " << -1 * elem.first << "\n";
+    }
+
+    return 0;
+}
