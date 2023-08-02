1cd74d7d69759a4a8982474030a052add09249ad
abrar-nazib <abrarnazib@gmail.com>
2023-08-02 19:35:13 +0600

    pre-computation start

---
+++ dsa/graph/pre_computation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//-------- typedefs -------
+typedef unsigned int ui;
+typedef unsigned long long llu;
+//------- /typedefs--------
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  return 0;
+}
