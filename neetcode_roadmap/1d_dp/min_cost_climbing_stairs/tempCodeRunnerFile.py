942eb9f8cc23203494b81ed51eacd88f0f9d1250
abrar-nazib <abrarnazib@gmail.com>
2025-04-03 12:48:53 +0600

    Solve 86th problem

--- neetcode_roadmap/1d_dp/min_cost_climbing_stairs/tempCodeRunnerFile.py
+++ neetcode_roadmap/1d_dp/min_cost_climbing_stairs/tempCodeRunnerFile.py
@@ -1,21 +1,4 @@
-from typing import List
-
-
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost = cost + [0, 0]
-        # print(cost)
-        for i in range(len(cost) - 4, -1, -1):
-            cost[i] = min(cost[i] + cost[i + 1], cost[i] + cost[i + 2])
-        return min(cost[0], cost[1])
-
-
-soln = Solution()
-cost = [10, 15, 20]
-print(soln.minCostClimbingStairs(cost))
-cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
-print(soln.minCostClimbingStairs(cost))
-cost = [
+ost = [
     841,
     462,
     566,
